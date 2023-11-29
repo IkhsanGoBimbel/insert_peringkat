9c2a2733128736b04439b4dabf40047c02018cbd
Ikhsan <ikhsan.gobimbel@gmail.com>
2023-11-29 09:00:24 +0700

    change kodingan dikit

--- src/app.service.ts
+++ src/app.service.ts
@@ -96,34 +96,63 @@ export class AppService {
 
       let data_fix: any[] = [];
       let data_no_bundling: any[] = [];
-      
-      const findData = async (index,item: any) => {
+
+      const findData = async (index, item: any) => {
         return new Promise<void>((resolve) => {
           setTimeout(async () => {
             const dobel = await this.prisma.t_produk_siswa.findMany({
               where: {
                 c_no_register: +item.c_no_register,
+                c_tahun_ajaran: '2023/2024',
               },
             });
 
             const cari = await this.prisma.t_produk_siswa.findFirst({
               where: {
                 c_no_register: +item.c_no_register,
+                c_tahun_ajaran: '2023/2024',
               },
             });
-            console.log(index+1)
-      
-            if (cari && dobel.length == 1) {
-              // Add a new column c_id_bundling to the item
-              const newItem = {
-                ...item,
-                c_id_bundling: cari.c_id_bundling,
-              };
-              // Push the new item to data_fix
-              data_fix.push(newItem);
-              console.log(newItem)
+            console.log(index + 1);
 
-            } else {
+            if (cari && dobel.length >= 1) {
+              // Add a new column c_id_bundling to the item
+              let newItem;
+
+              if (dobel.length == 1) {
+                newItem = {
+                  ...item,
+                  c_id_bundling: cari.c_id_bundling,
+                };
+                data_fix.push(newItem);
+              } else {
+                dobel.map((siswa) => {
+                  newItem = {
+                    c_no_register: item.c_no_register,
+                    c_nama_lengkap: item.c_nama_lengkap,
+                    c_total: item.c_total,
+                    c_id_sekolah_kelas: siswa.c_id_sekolah_kelas,
+                    c_id_kota: item.c_id_kota,
+                    c_nama_kota: item.c_nama_kota,
+                    c_id_gedung: item.c_id_gedung,
+                    c_nama_gedung: item.c_nama_gedung,
+                    c_tahun_ajaran: item.c_tahun_ajaran,
+                    c_benarlevel1: item.c_benarlevel1,
+                    c_benarlevel2: item.c_benarlevel2,
+                    c_benarlevel3: item.c_benarlevel3,
+                    c_benarlevel4: item.c_benarlevel4,
+                    c_benarlevel5: item.c_benarlevel5,
+                    c_salahlevel1: item.c_salahlevel1,
+                    c_salahlevel2: item.c_salahlevel2,
+                    c_salahlevel3: item.c_salahlevel3,
+                    c_salahlevel4: item.c_salahlevel4,
+                    c_salahlevel5: item.c_salahlevel5,
+                    c_id_bundling: siswa.c_id_bundling,
+                  };
+                  data_fix.push(newItem);
+                });
+              }
+            } else if (!cari && dobel.length == 0) {
               // Add a new column c_id_bundling with null value to the item
               const OldItem = {
                 ...item,
@@ -131,14 +160,14 @@ export class AppService {
               };
               // Push the item with null c_id_bundling to data_no_bundling
               data_no_bundling.push(OldItem);
-              console.log('tidak ada bundling')
+              console.log('tidak ada bundling');
             }
-      
+
             resolve();
           }, 1); // Set the delay in milliseconds (1 detik)
         });
       };
-      
+
       // Process each data item with a delay
       for (const [index, item] of data.entries()) {
         await findData(index, item);
@@ -184,27 +213,26 @@ export class AppService {
           c_id_gedung: items.c_id_gedung,
           c_nama_gedung: items.c_nama_gedung,
           c_tahun_ajaran: items.c_tahun_ajaran,
-          c_benarlevel1:  items.c_benarlevel1,
-          c_benarlevel2:  items.c_benarlevel2,
-          c_benarlevel3:  items.c_benarlevel3,
-          c_benarlevel4:  items.c_benarlevel4,
-          c_benarlevel5:  items.c_benarlevel5,
-          c_salahlevel1:  items.c_salahlevel1,
-          c_salahlevel2:  items.c_salahlevel2,
-          c_salahlevel3:  items.c_salahlevel3,
-          c_salahlevel4:  items.c_salahlevel4,
-          c_salahlevel5:  items.c_salahlevel5,
-          c_id_bundling:  items.c_id_bundling
+          c_benarlevel1: items.c_benarlevel1,
+          c_benarlevel2: items.c_benarlevel2,
+          c_benarlevel3: items.c_benarlevel3,
+          c_benarlevel4: items.c_benarlevel4,
+          c_benarlevel5: items.c_benarlevel5,
+          c_salahlevel1: items.c_salahlevel1,
+          c_salahlevel2: items.c_salahlevel2,
+          c_salahlevel3: items.c_salahlevel3,
+          c_salahlevel4: items.c_salahlevel4,
+          c_salahlevel5: items.c_salahlevel5,
+          c_id_bundling: items.c_id_bundling,
         });
       });
 
-      
       const filePathBaru = `${file.filename} data_fix_bundling.xlsx`;
       await workbookBaru.xlsx.writeFile(filePathBaru);
 
-
       const workbookBaruNoBundling = new ExcelJS.Workbook();
-      const worksheetBaruBaruNoBundling = workbookBaruNoBundling.addWorksheet('Data_no_bundling');
+      const worksheetBaruBaruNoBundling =
+        workbookBaruNoBundling.addWorksheet('Data_no_bundling');
 
       // Menentukan header kolom
       worksheetBaruBaruNoBundling.columns = [
@@ -231,7 +259,7 @@ export class AppService {
       ];
 
       data_no_bundling.forEach((items) => {
-        worksheetBaru.addRow({
+        worksheetBaruBaruNoBundling.addRow({
           c_no_register: items.c_no_register,
           c_nama_lengkap: items.c_nama_lengkap,
           c_total: items.c_total,
@@ -241,24 +269,25 @@ export class AppService {
           c_id_gedung: items.c_id_gedung,
           c_nama_gedung: items.c_nama_gedung,
           c_tahun_ajaran: items.c_tahun_ajaran,
-          c_benarlevel1:  items.c_benarlevel1,
-          c_benarlevel2:  items.c_benarlevel2,
-          c_benarlevel3:  items.c_benarlevel3,
-          c_benarlevel4:  items.c_benarlevel4,
-          c_benarlevel5:  items.c_benarlevel5,
-          c_salahlevel1:  items.c_salahlevel1,
-          c_salahlevel2:  items.c_salahlevel2,
-          c_salahlevel3:  items.c_salahlevel3,
-          c_salahlevel4:  items.c_salahlevel4,
-          c_salahlevel5:  items.c_salahlevel5,
-          c_id_bundling:  items.c_id_bundling
+          c_benarlevel1: items.c_benarlevel1,
+          c_benarlevel2: items.c_benarlevel2,
+          c_benarlevel3: items.c_benarlevel3,
+          c_benarlevel4: items.c_benarlevel4,
+          c_benarlevel5: items.c_benarlevel5,
+          c_salahlevel1: items.c_salahlevel1,
+          c_salahlevel2: items.c_salahlevel2,
+          c_salahlevel3: items.c_salahlevel3,
+          c_salahlevel4: items.c_salahlevel4,
+          c_salahlevel5: items.c_salahlevel5,
+          c_id_bundling: items.c_id_bundling,
         });
       });
 
       const filePathBaruNoBundling = `${file.filename} data_no_bundling.xlsx`;
       await workbookBaruNoBundling.xlsx.writeFile(filePathBaruNoBundling);
 
-
+      console.log(`data fix bundling = ${data_fix.length}`);
+      console.log(`data no bundling = ${data_no_bundling.length}`);
 
       if (fs.existsSync(filePath)) {
         fs.unlinkSync(filePath); // Menghapus file jika ada
